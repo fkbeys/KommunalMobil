2494678ba4c830df0689c4fc640e8f925df50d5f
fkbeys <fkbeys@gmail.com>
2022-12-31 15:34:12 +0400

    initial
    initial

---
+++ src/Pages/DashBoard.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+const DashBoard = () => {
+    return (
+        <Text>DashBoard</Text>
+    )
+}
+
+export default DashBoard
---
+++ src/Pages/LoginPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+const LoginPage = () => {
+    return (
+        <Text>LoginPage</Text>
+    )
+}
+
+export default LoginPage
